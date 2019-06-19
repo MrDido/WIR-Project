29af2ea2c839664a20c2b078f74249a4f5b7a226
Matteo <matteo2795@gmail.com>
2019-06-19 09:43:13 +0200

    Removed template and wikipedia pages

--- remove_User_or_Talk.py
+++ remove_User_or_Talk.py
@@ -2,13 +2,13 @@ import wikipediaapi
 import io
 wiki=wikipediaapi.Wikipedia('en')
 
-t = wiki.page("Tea")
+t = wiki.page("Hiking")
 d = t.backlinks
 
 def removeUser(d):
     res=[]
     for i in d.keys():
-        if ("user" not in i.lower()) and ("talk" not in i.lower()):
+        if ("user" not in i.lower()) and ("talk" not in i.lower()) and ("wikipedia" not in i.lower() ) and ("template" not in i.lower()):
             res.append(i)
     return res
 
@@ -19,4 +19,4 @@ with open("output_Tea.txt", "w", encoding="utf-8") as f:
     for i in new:
         f.write(i +"\n")
         print(i)
-
+print(len(new))
